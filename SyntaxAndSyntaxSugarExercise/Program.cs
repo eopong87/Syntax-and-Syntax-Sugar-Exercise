14b07ddbd48f860a8f7431706e56e5a4b2d01624
eopong87 <eopong87@gmail.com>
2023-06-22 14:55:39 -0400

    sugar

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -5,6 +5,24 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            //int answer = 4;// explicit typing
+            //string response;// explicit
+
+            //if (answer < 9)
+            //{
+              //  response = answer + " is less than nine";
+            //}
+            //else
+            //{
+              //  response = answer + "greater than or equal to nine";
+            //}
+            //Console.WriteLine(response);
+
+
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"greater than or ewual to nine";
+            Console.WriteLine(response);
         }
     }
 }
